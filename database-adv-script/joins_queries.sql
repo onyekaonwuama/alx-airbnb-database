3ab32e8fd6dde061dafb72fda52bb66737a029e8
Onyeka <nnyeka212@gmail.com>
2025-07-02 13:36:10 +0200

    Modified inner join sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,14 +1,12 @@
 SELECT
-Booking.booking_id,
-Booking.start_date,
-Booking.end_date,
-Booking.total_price,
-User.user_id,
-User.first_name,
-User.last_name,
-User.email
+    Booking.booking_id,
+    Booking.start_date,
+    Booking.end_date,
+    Booking.total_price,
+    User.user_id,
+    User.first_name,
+    User.last_name,
+    User.email
 FROM
-Booking
-INNER JOIN User
-ON
-Booking.user_id = User.user_id;
+    Booking
+    INNER JOIN User ON Booking.user_id = User.user_id;
