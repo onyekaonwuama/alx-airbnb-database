1480217b50e4b0990f31663df410be1741882d80
Onyeka <nnyeka212@gmail.com>
2025-07-02 13:41:08 +0200

    Added left join queries

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,3 +1,5 @@
+--inner join Retrieve all bookings and the respective users who made those bookings
+
 SELECT
     Booking.booking_id,
     Booking.start_date,
@@ -10,3 +12,17 @@ SELECT
 FROM
     Booking
     INNER JOIN User ON Booking.user_id = User.user_id;
+
+--left join Retrieve all properties and their reviews, including properties with no reviews
+SELECT
+Property.property_id,
+Property.name,
+Property.location,
+Property.pricepernight,
+Review.review_id,
+Review.rating,
+Review.comment
+FROM
+Property
+LEFT JOIN Review
+ON Property.property_id = Review.property_id
