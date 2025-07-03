eea4de6656d95b7ae9a1ed11bd887c3adf107808
Onyeka <nnyeka212@gmail.com>
2025-07-03 20:55:30 +0200

    Added expalin analyse to monitor the performance of optimized query

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -101,3 +101,30 @@ JOIN
     Property ON Booking.property_id = Property.property_id
 LEFT JOIN 
     Payment ON Booking.booking_id = Payment.booking_id;
+
+-- Step 5: Measure the performance of the optimized query using EXPLAIN ANALYZE (after optimization)
+EXPLAIN ANALYZE 
+SELECT 
+    Booking.booking_id,
+    Booking.start_date,
+    Booking.end_date,
+    Booking.total_price,
+    User.user_id,
+    User.first_name,
+    User.last_name,
+    User.email,
+    Property.property_id,
+    Property.name AS property_name,
+    Property.location AS property_location,
+    Property.pricepernight,
+    Payment.payment_id,
+    Payment.amount AS payment_amount,
+    Payment.payment_date
+FROM 
+    Booking
+JOIN 
+    User ON Booking.user_id = User.user_id
+JOIN 
+    Property ON Booking.property_id = Property.property_id
+LEFT JOIN 
+    Payment ON Booking.booking_id = Payment.booking_id;
